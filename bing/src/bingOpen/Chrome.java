25cf0295ca27c97a5b71d6082dcedc9ce8450e99
raj-QA <rajkumaryin28@gmail.com>
2022-06-19 01:03:18 +0530

    first

---
+++ bing/src/bingOpen/Chrome.java
@@ -0,0 +1,17 @@
+package bingOpen;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Chrome {
+	
+	public static void main(String[] args) throws Exception {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\USER\\Downloads\\chromedriver89\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https:\\www.google.com");
+		driver.manage().window().maximize();
+	    driver.close();
+		
+	}
+
+}
