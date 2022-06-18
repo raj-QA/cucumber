8d09aae7156713710c399bef9ee1831e01ae0e00
raj-QA <rajkumaryin28@gmail.com>
2022-06-19 00:40:28 +0530

    fi

---
+++ elakiya/src/Indhu/Box.java
@@ -0,0 +1,20 @@
+package Indhu;
+
+class WoodBox{
+	int length;
+}
+
+public class Box {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WoodBox Bo;
+		Bo = new WoodBox();
+		Bo.length = 20;
+		System.out.println(Bo.length);
+		
+
+	}
+
+}
